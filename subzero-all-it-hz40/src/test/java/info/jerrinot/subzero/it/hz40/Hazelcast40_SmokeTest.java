2fed7b794910fe91836b573c4df1e6c68ecf1f14
Tomasz Gaweda <tgaweda@codedose.com>
2019-11-06 13:29:44 +0100

    #39 Support for Hazelcast 4.0

---
+++ subzero-all-it-hz40/src/test/java/info/jerrinot/subzero/it/hz40/Hazelcast40_SmokeTest.java
@@ -0,0 +1,6 @@
+package info.jerrinot.subzero.it.hz40;
+
+import info.jerrinot.subzero.it.BaseSmokeTests;
+
+public class Hazelcast40_SmokeTest extends BaseSmokeTests {
+}
--- subzero-core/src/main/java/info/jerrinot/subzero/internal/strategy/DelegatingClassResolver.java
+++ subzero-core/src/main/java/info/jerrinot/subzero/internal/strategy/DelegatingClassResolver.java
@@ -1,7 +1,9 @@
 package info.jerrinot.subzero.internal.strategy;
 
 import com.esotericsoftware.kryo.util.DefaultClassResolver;
-import com.hazelcast.nio.ClassLoaderUtil;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Delegates class loading to Hazelcast -> SubZero will use the same strategy
@@ -11,6 +13,7 @@ import com.hazelcast.nio.ClassLoaderUtil;
 final class DelegatingClassResolver extends DefaultClassResolver {
 
     private final ClassLoader classLoader;
+    private static final Method loadClassMethodRef = getClassLoadMethod();
 
     public DelegatingClassResolver(ClassLoader classLoader) {
         this.classLoader = classLoader;
@@ -19,8 +22,45 @@ final class DelegatingClassResolver extends DefaultClassResolver {
     @Override
     protected Class<?> getTypeByName(String className) {
         try {
-            return ClassLoaderUtil.loadClass(classLoader, className);
+            return (Class<?>) loadClassMethodRef.invoke(null, classLoader, className);
+        } catch (InvocationTargetException e) {
+            if (e.getCause() instanceof ClassNotFoundException) {
+                return null;
+            }
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Method getClassLoadMethod() {
+        Class<?> classLoaderUtilClass = getClassLoaderUtilClass();
+        try {
+            return classLoaderUtilClass.getDeclaredMethod("loadClass", ClassLoader.class, String.class);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static Class<?> getClassLoaderUtilClass() {
+        String[] possibleNames = {
+                "com.hazelcast.nio.ClassLoaderUtil",
+                "com.hazelcast.internal.nio.ClassLoaderUtil"
+        };
+
+        for (String possibleName : possibleNames) {
+            Class<?> loadedClass = loadClass(possibleName);
+            if (loadedClass != null) return loadedClass;
+        }
+        throw new IllegalStateException("unable to load ClassLoaderUtil");
+    }
+
+    private static Class<?> loadClass(String name) {
+        try {
+            return Class.forName(name);
         } catch (ClassNotFoundException e) {
+            return null;
+        } catch (NoClassDefFoundError e) {
             return null;
         }
     }
